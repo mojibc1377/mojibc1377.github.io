669271190b304d4dec82af9b54bc5ae5e1d07b52
mojtaba <mojtaba@MacAroni.local>
2023-08-09 14:35:41 +0330

    rounded ready to deploy

--- src/pages/certificates.jsx
+++ src/pages/certificates.jsx
@@ -15,16 +15,16 @@ export default function Certificates(){
         <div className='info-part'>
             
               <header className='header animate-pulse'>Database and Sql for Data science</header>
-              <img src='/image/db-and-sql for ds.jpg' className={`cert-image ${imagesLoaded ? 'image-loaded ps-3' : 'loading-placeholder'}`} alt='db-sqlForDs'></img>
+              <img src='/image/db-and-sql for ds.jpg' className={`cert-image ${imagesLoaded ? 'image-loaded ps-3 rounded-md' : 'loading-placeholder'}`} alt='db-sqlForDs'></img>
               
               <header className='header animate-pulse'>Data science methodology</header>
-              <img src='/image/ds-methodology.jpg' className={`cert-image ${imagesLoaded ? 'image-loaded ps-3' : 'loading-placeholder'}`} alt='Data science methodology'></img>
+              <img src='/image/ds-methodology.jpg' className={`cert-image ${imagesLoaded ? 'image-loaded ps-3 rounded-md' : 'loading-placeholder'}`} alt='Data science methodology'></img>
               
               <header className='header animate-pulse'>Python for Data science and AI development</header>
-              <img src='/image/python-for-ds-and-ai-development.jpg' className={`cert-image ${imagesLoaded ? 'image-loaded ps-3' : 'loading-placeholder'}`} alt='python-for-ds-and-ai-development'></img>
+              <img src='/image/python-for-ds-and-ai-development.jpg' className={`cert-image ${imagesLoaded ? 'image-loaded ps-3 rounded-md' : 'loading-placeholder'}`} alt='python-for-ds-and-ai-development'></img>
              
               <header className='header animate-pulse'>Tools for Data science</header>
-              <img src='/image/tools-for-ds.jpg' className={`cert-image ${imagesLoaded ? 'image-loaded ps-3' : 'loading-placeholder'}`} alt='tools'></img>
+              <img src='/image/tools-for-ds.jpg' className={`cert-image ${imagesLoaded ? 'image-loaded ps-3 rounded-md' : 'loading-placeholder'}`} alt='tools'></img>
         </div>
     )
 }
